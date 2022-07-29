83711672c0f77abf3bb94f1555188dfa570b6213
Takeru Kondo <94258767+TakeruKondo@users.noreply.github.com>
2022-07-29 17:25:58 +0900

    Delete capp directory

--- capp/booking/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint,render_template
-
-bookingme = Blueprint('bookingme',__name__,template_folder="templates",static_folder="static")
-
-
-@bookingme.route('/booking.html')
-def booking():
-    return render_template('booking.html')
-
